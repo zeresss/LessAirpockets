87e7cb03205c24827d586131af382eed0d3d2764
Peter <petr.ovchinnicov@gmail.com>
2024-12-04 20:07:30 +0300

    initial commit

---
+++ src/main/java/com/zer/lessairpockets/mixin/CanPlaceAtMixin.java
@@ -0,0 +1,28 @@
+package com.zer.lessairpockets.mixin;
+
+import net.minecraft.block.*;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.ViewableWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = {
+    TorchBlock.class, WallTorchBlock.class, PlantBlock.class, MushroomPlantBlock.class,
+    RedstoneWireBlock.class, AbstractRedstoneGateBlock.class, SnowBlock.class
+})
+public abstract class CanPlaceAtMixin {
+
+    /**
+     * makes it impossible to place block in any fluid
+     */
+    @Inject(method = "canPlaceAt", at = @At("RETURN"), cancellable = true)
+    private void canPlaceAt(BlockState state, ViewableWorld world, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (this instanceof NewUnplaceable) {
+            cir.setReturnValue(cir.getReturnValue() && world.getFluidState(pos).getFluid() == Fluids.EMPTY);
+        }
+    }
+
+}
